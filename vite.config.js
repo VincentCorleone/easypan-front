c0e5b8ab59c36d5971ac851072e4171da51f034d
VincentCorleone <707119577@qq.com>
2023-11-07 21:43:38 +0800

    配置开发环境代理绕过跨站限制

--- vite.config.js
+++ vite.config.js
@@ -12,5 +12,17 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  server: { 
+    //用来配置跨域
+    host: '127.0.0.1',
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:8080',//目标服务器地址
+        changeOrigin: true,
+        // rewrite: (path) => path.replace(/^\/api/, '') 
+      },
+    }
   }
 })
