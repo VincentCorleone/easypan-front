7be489b694738b15121d696ee4044354e432362c
VincentCorleone <707119577@qq.com>
2023-12-19 23:03:23 +0800

    完成文件上传前端页面

--- src/views/Files.vue
+++ src/views/Files.vue
@@ -1,3 +1,26 @@
 <template>
-    文件列表
+    <el-upload
+    :http-request="upload">
+        <el-button type="primary">
+            <span class="iconfont icon-upload"></span>
+            Primary
+        </el-button>
+    </el-upload>
 </template>
+
+<script setup>
+import { getCurrentInstance } from 'vue'
+
+const { proxy } = getCurrentInstance()
+
+const upload = (file)=>{
+    console.log(file.file)
+    proxy.Request.post("/file/upload",{
+        file: file.file
+    },{
+        headers: {
+            'Content-Type': 'multipart/form-data'
+        }
+    })
+}
+</script>
