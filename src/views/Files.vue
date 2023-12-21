daedd7508f9378447b46691ca3a9729591bd244c
VincentCorleone <707119577@qq.com>
2023-12-21 19:08:20 +0800

    展示文件列表

--- src/views/Files.vue
+++ src/views/Files.vue
@@ -6,21 +6,59 @@
             Primary
         </el-button>
     </el-upload>
+
+    <el-table :data="files" style="width: 100%">
+        <el-table-column prop="fileName" label="文件名" width="180" />
+        <el-table-column prop="lastModified" label="最近修改时间" width="180" />
+        <el-table-column prop="size" label="大小" />
+    </el-table>
 </template>
 
 <script setup>
-import { getCurrentInstance } from 'vue'
+import { getCurrentInstance,ref } from 'vue'
 
 const { proxy } = getCurrentInstance()
 
+const files = ref([])
+
+
+import { onMounted } from 'vue'
+import { ElMessage } from 'element-plus'
+
+const loadFiles = ()=> {
+    proxy.Request.get("/file/loadFiles",{
+        params: {
+            currentPath: "/"
+        }
+    
+    }).then((response)=>{
+        console.log(response.data)
+        //   ElMessage({
+        //       message: response.data.message,
+        //       type: 'success',
+        //   })
+        files.value = response.data
+      }).catch(function (error) {
+        //   ElMessage({
+        //       message: error.response.data.message,
+        //       type: 'error',
+        //   })
+      })
+}
+
+onMounted(loadFiles)
+
 const upload = (file)=>{
-    console.log(file.file)
     proxy.Request.post("/file/upload",{
         file: file.file
     },{
         headers: {
             'Content-Type': 'multipart/form-data'
         }
+    }).then((response)=>{
+        loadFiles()
     })
 }
+
+
 </script>
