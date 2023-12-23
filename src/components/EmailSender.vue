21bfcde28f3d8b78606439cd797e9cb256da6d95
VincentCorleone <707119577@qq.com>
2023-12-23 21:39:15 +0800

    美化文件格式

--- src/components/EmailSender.vue
+++ src/components/EmailSender.vue
@@ -1,139 +1,145 @@
 <template>
-    <el-dialog :model-value="dialogVisible" title="获取邮箱验证码" @close="close">
-        <div>
-            <span>邮箱</span> <span>{{ email }}</span>
-        </div>
-        <div class="checkCodeWrapper">
-            <el-form :model="form" :rules="formRules" ref="formRef">
-                <el-form-item prop="captcha">
-                    <el-input size="large" placeholder="请输入验证码" v-model="form.captcha">
-                        <template #prefix>
-                            <el-icon :size="18">
-                                <Picture />
-                            </el-icon>
-                        </template>
-                    </el-input>
-                </el-form-item>
-            </el-form>
-            <img class="checkCode" :src="captchaUrlEmail" @click="updateCaptcha('email')">
-        </div>
-        <template #footer>
-            <el-button type="primary" @click="getEmailCode(formRef)">发送验证码</el-button>
-        </template>
-    </el-dialog>
+  <el-dialog :model-value="dialogVisible" title="获取邮箱验证码" @close="close">
+    <div>
+      <span>邮箱</span> <span>{{ email }}</span>
+    </div>
+    <div class="checkCodeWrapper">
+      <el-form :model="form" :rules="formRules" ref="formRef">
+        <el-form-item prop="captcha">
+          <el-input
+            size="large"
+            placeholder="请输入验证码"
+            v-model="form.captcha"
+          >
+            <template #prefix>
+              <el-icon :size="18">
+                <Picture />
+              </el-icon>
+            </template>
+          </el-input>
+        </el-form-item>
+      </el-form>
+      <img
+        class="checkCode"
+        :src="captchaUrlEmail"
+        @click="updateCaptcha('email')"
+      />
+    </div>
+    <template #footer>
+      <el-button type="primary" @click="getEmailCode(formRef)"
+        >发送验证码</el-button
+      >
+    </template>
+  </el-dialog>
 </template>
 
 <script setup>
-import {ref,reactive, getCurrentInstance, nextTick} from 'vue'
-import { ElMessage, ElLoading } from 'element-plus'
-
+import { ref, reactive, getCurrentInstance, nextTick } from "vue";
+import { ElMessage, ElLoading } from "element-plus";
 
 const { proxy } = getCurrentInstance();
 
+import config from "../utils/config.js";
 
-import config from '../utils/config.js'
-
-const baseUrl = config.baseUrl
+const baseUrl = config.baseUrl;
 const api = {
-  captcha: "/captcha"
-}
+  captcha: "/captcha",
+};
 
 const validatorCaptcha = (rule, value, callback) => {
-    if (!/^[A-Za-z0-9]{5}$/.test(value)) return callback(new Error("验证码格式不正确"));
-    callback();
+  if (!/^[A-Za-z0-9]{5}$/.test(value))
+    return callback(new Error("验证码格式不正确"));
+  callback();
 };
 
 const formRules = {
-    captcha: [
-        { required: true, message: "验证码不能为空" },
-        { validator: validatorCaptcha }
-    ]
-}
+  captcha: [
+    { required: true, message: "验证码不能为空" },
+    { validator: validatorCaptcha },
+  ],
+};
 
+const props = defineProps(["dialogVisible", "email", "api"]);
+const emit = defineEmits(["close"]);
 
+const form = reactive({});
 
-const props = defineProps(['dialogVisible','email','api'])
-const emit = defineEmits(['close'])
+const captchaUrlEmail = ref(
+  baseUrl + api.captcha + "?type=email&time=" + new Date().getTime()
+);
 
-const form = reactive({})
-
-const captchaUrlEmail = ref(baseUrl + api.captcha + "?type=email&time=" + new Date().getTime())
-
-const formRef = ref(null)
+const formRef = ref(null);
 
 const close = () => {
-    emit('close')
-}
+  emit("close");
+};
 
-
-const getEmailCode = (formEl)=>{
-    formEl.validate((valid)=>{
-
-        console.log(valid)
-        if(valid){
-            console.log(props)
-            const loading = ElLoading.service({
-                text: '正在发送邮件验证码'
-            })
-            proxy.Request.get(props.api,{
-                params: {
-                    email: props.email,
-                    captcha: form.captcha
-                }
-            }).then((response)=>{
-                nextTick(() => {
-                    loading.close()
-                })
-                ElMessage({
-                    message: response.data.message,
-                    type: 'success',
-                })
-                close();
-            }).catch(function (error) {
-                nextTick(() => {
-                    loading.close()
-                })
-                console.log(error)
-                ElMessage({
-                    message: error.response.data.message,
-                    type: 'error',
-                })
-                updateCaptcha('email')
-            })
-
-        }
-        
-    })
-    
-}
-
-const updateCaptcha = (type)=>{
-    console.log("更新验证码")
-    if(type == "email"){
-        captchaUrlEmail.value = baseUrl + api.captcha + "?type=email&time=" + new Date().getTime()
-    }else{
-        captchaUrl.value = baseUrl + api.captcha + "?time=" + new Date().getTime()
+const getEmailCode = (formEl) => {
+  formEl.validate((valid) => {
+    console.log(valid);
+    if (valid) {
+      console.log(props);
+      const loading = ElLoading.service({
+        text: "正在发送邮件验证码",
+      });
+      proxy.Request.get(props.api, {
+        params: {
+          email: props.email,
+          captcha: form.captcha,
+        },
+      })
+        .then((response) => {
+          nextTick(() => {
+            loading.close();
+          });
+          ElMessage({
+            message: response.data.message,
+            type: "success",
+          });
+          close();
+        })
+        .catch(function (error) {
+          nextTick(() => {
+            loading.close();
+          });
+          console.log(error);
+          ElMessage({
+            message: error.response.data.message,
+            type: "error",
+          });
+          updateCaptcha("email");
+        });
     }
-}
+  });
+};
 
-import { onUpdated } from 'vue'
+const updateCaptcha = (type) => {
+  console.log("更新验证码");
+  if (type == "email") {
+    captchaUrlEmail.value =
+      baseUrl + api.captcha + "?type=email&time=" + new Date().getTime();
+  } else {
+    captchaUrl.value = baseUrl + api.captcha + "?time=" + new Date().getTime();
+  }
+};
+
+import { onUpdated } from "vue";
 
 onUpdated(() => {
-    updateCaptcha('email')
-})
-
+  updateCaptcha("email");
+});
 </script>
 
 <style scoped>
-.checkCodeWrapper{
+.checkCodeWrapper {
   display: flex;
   width: 100%;
-  .checkCode{
+  .checkCode {
     height: 40px;
     margin-left: 5px;
   }
 
-  .getEmailCode{
+  .getEmailCode {
     margin-left: 10px;
   }
 }
--- src/views/Files.vue
+++ src/views/Files.vue
@@ -1,64 +1,65 @@
 <template>
-    <el-upload
-    :http-request="upload">
-        <el-button type="primary">
-            <span class="iconfont icon-upload"></span>
-            Primary
-        </el-button>
-    </el-upload>
+  <el-upload :http-request="upload">
+    <el-button type="primary">
+      <span class="iconfont icon-upload"></span>
+      Primary
+    </el-button>
+  </el-upload>
 
-    <el-table :data="files" style="width: 100%">
-        <el-table-column prop="fileName" label="文件名" width="180" />
-        <el-table-column prop="lastModified" label="最近修改时间" width="180" />
-        <el-table-column prop="size" label="大小" />
-    </el-table>
+  <el-table :data="files" style="width: 100%">
+    <el-table-column prop="fileName" label="文件名" width="180" />
+    <el-table-column prop="lastModified" label="最近修改时间" width="180" />
+    <el-table-column prop="size" label="大小" />
+  </el-table>
 </template>
 
 <script setup>
-import { getCurrentInstance,ref } from 'vue'
+import { getCurrentInstance, ref } from "vue";
 
-const { proxy } = getCurrentInstance()
+const { proxy } = getCurrentInstance();
 
-const files = ref([])
+const files = ref([]);
 
+import { onMounted } from "vue";
+import { ElMessage } from "element-plus";
 
-import { onMounted } from 'vue'
-import { ElMessage } from 'element-plus'
-
-const loadFiles = ()=> {
-    proxy.Request.get("/file/loadFiles",{
-        params: {
-            currentPath: "/"
-        }
-    
-    }).then((response)=>{
-        console.log(response.data)
-        //   ElMessage({
-        //       message: response.data.message,
-        //       type: 'success',
-        //   })
-        files.value = response.data
-      }).catch(function (error) {
-        //   ElMessage({
-        //       message: error.response.data.message,
-        //       type: 'error',
-        //   })
-      })
-}
-
-onMounted(loadFiles)
-
-const upload = (file)=>{
-    proxy.Request.post("/file/upload",{
-        file: file.file
-    },{
-        headers: {
-            'Content-Type': 'multipart/form-data'
-        }
-    }).then((response)=>{
-        loadFiles()
+const loadFiles = () => {
+  proxy.Request.get("/file/loadFiles", {
+    params: {
+      currentPath: "/",
+    },
+  })
+    .then((response) => {
+      console.log(response.data);
+      //   ElMessage({
+      //       message: response.data.message,
+      //       type: 'success',
+      //   })
+      files.value = response.data.data;
     })
-}
+    .catch(function (error) {
+      //   ElMessage({
+      //       message: error.response.data.message,
+      //       type: 'error',
+      //   })
+    });
+};
 
+onMounted(loadFiles);
 
+const upload = (file) => {
+  proxy.Request.post(
+    "/file/upload",
+    {
+      file: file.file,
+    },
+    {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    }
+  ).then((response) => {
+    loadFiles();
+  });
+};
 </script>
